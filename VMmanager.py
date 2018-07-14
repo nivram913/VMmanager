44058a6ea2c4b3e8eb0ada41ae8033882d20838f
Marvin V <wmarvin035@gmail.com>
2018-07-14 14:12:02 +0200

    Add help messages

--- VMmanager.py
+++ VMmanager.py
@@ -51,7 +51,7 @@ class VMmanager:
         parser = argparse.ArgumentParser(prog='list', description='List all VMs')
         parser.add_argument('-c', dest='config', action='store_true', help='Include configuration')
         parser.add_argument('-s', dest='status', action='store_true', help='Include status')
-        parser.add_argument('name', nargs='?', default='', type=self._validate_vm_name)
+        parser.add_argument('name', nargs='?', default='', type=self._validate_vm_name, help='Existing VM name')
         args = parser.parse_args(args)
 
         if args.name != '':
@@ -69,7 +69,7 @@ class VMmanager:
 
     def create(self, args):
         parser = argparse.ArgumentParser(prog='create', description='Create a new VM')
-        parser.add_argument('name', nargs=1, type=self._validate_vm_name)
+        parser.add_argument('name', nargs=1, type=self._validate_vm_name, help='VM name')
         parser.add_argument('--disk', required=True, help='Disk size (understand suffix M and G)')
         parser.add_argument('--ram', required=True, help='Memory size (understand suffix M and G)')
         parser.add_argument('--cdrom', required=False, help='Iso file to put in virtual CD-ROM')
@@ -78,7 +78,7 @@ class VMmanager:
 
     def modify(self, args):
         parser = argparse.ArgumentParser(prog='modify', description='Modify an existing VM')
-        parser.add_argument('name', nargs=1, type=self._validate_vm_name)
+        parser.add_argument('name', nargs=1, type=self._validate_vm_name, help='VM name')
         parser.add_argument('--ram', required=False, help='Memory size (understand suffix M and G)')
         parser.add_argument('--cdrom', required=False, help='Iso file to put in virtual CD-ROM')
         parser.add_argument('--network', required=False, choices=['none', 'NAT', 'bridge'], help='Network type')
@@ -86,31 +86,31 @@ class VMmanager:
 
     def delete(self, args):
         parser = argparse.ArgumentParser(prog='delete', description='Delete an existing VM')
-        parser.add_argument('name', nargs=1, type=self._validate_vm_name)
+        parser.add_argument('name', nargs=1, type=self._validate_vm_name, help='Existing VM name')
         parser.add_argument('-f', dest='force', action='store_true', help='Force operation if VM is running')
         parser.add_argument('--preserve-disk', action='store_true', help="Don't delete disk")
         args = parser.parse_args(args)
 
     def state(self, args):
         parser = argparse.ArgumentParser(prog='state', description='Get state of all/a running VM')
-        parser.add_argument('name', nargs='?', default='', type=self._validate_vm_name)
+        parser.add_argument('name', nargs='?', default='', type=self._validate_vm_name, help='Existing VM name')
         args = parser.parse_args(args)
 
     def snapshot(self, args):
         parser = argparse.ArgumentParser(prog='snapshot', description='Take a snapshot of a stopped VM')
-        parser.add_argument('name', nargs=1, type=self._validate_vm_name)
-        parser.add_argument('snapshot_name', nargs=1)
+        parser.add_argument('name', nargs=1, type=self._validate_vm_name, help='Existing VM name')
+        parser.add_argument('snapshot_name', nargs=1, help='Snapshot file name')
         args = parser.parse_args(args)
 
     def run(self, args):
         parser = argparse.ArgumentParser(prog='run', description='Launch a VM')
-        parser.add_argument('name', nargs=1, type=self._validate_vm_name)
+        parser.add_argument('name', nargs=1, type=self._validate_vm_name, help='Existing VM name')
         parser.add_argument('--boot', required=False, choices=['cdrom', 'disk'], help="Select boot device")
         args = parser.parse_args(args)
 
     def stop(self, args):
         parser = argparse.ArgumentParser(prog='stop', description='Stop a running VM')
-        parser.add_argument('name', nargs=1, type=self._validate_vm_name)
+        parser.add_argument('name', nargs=1, type=self._validate_vm_name, help='Existing VM name')
         parser.add_argument('-f', dest='force', action='store_true', help='Force operation')
         args = parser.parse_args(args)
 
